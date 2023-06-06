1d1aef042dcbdc470cace5fe11c23200ce032189
Ivo-Aragao <105293872+Ivo-Aragao@users.noreply.github.com>
2023-06-06 16:37:40 -0300

    app.js

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const tutorRoutes = require('./routes/tutor');
 const usuarioRoutes = require('./routes/usuario-Id');
 const petRoutes = require('./routes/pet');
 
-app.use('/pet', petRoutes);
-
 app.use(express.json());
 
 app.use(cors({ origin: true, credentials: true }));
@@ -14,7 +12,7 @@ app.use(cors({ origin: true, credentials: true }));
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token, Authorization');
   if (req.method === "OPTIONS") {
     return res.status(200).end();
@@ -39,6 +37,7 @@ app.get('/', (req, res) => {
 
 app.use('/tutor', tutorRoutes);
 app.use('/usuario-Id', usuarioRoutes);
+app.use('/pet', petRoutes);
 
 app.get('*', (req, res, next) => {
   setImmediate(() => {
@@ -54,3 +53,4 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
   console.log('Servidor rodando em: http://localhost:3000');
 });
+
