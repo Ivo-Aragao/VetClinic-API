3bee84a6057980d632d1377be0a339adea13a56b
Ivo-Aragao <105293872+Ivo-Aragao@users.noreply.github.com>
2023-06-06 11:55:52 -0300

    tutor.js

--- routes/tutor.js
+++ routes/tutor.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-let users = [[
-  {
-"id": 1,
-"name": "Jonh Doe",
-"phone": "85989323895",
-"email": "jose.abreu@compasso.com",
-"date_of_birth": "1993-12-12 10:10",
-"zip_code": "61760000",
-"pets": [
+let users = [
   {
     "id": 1,
-    "name": "Lilo",
-    "species": "dog",
-    "carry": "p",
-    "weight": 5,
-    "date_of_birth": "1993-12-12 10:10"
+    "name": "Jonh Doe",
+    "phone": "85989323895",
+    "email": "jose.abreu@compasso.com",
+    "date_of_birth": "1993-12-12 10:10",
+    "zip_code": "61760000",
+    "pets": [
+      {
+        "id": 1,
+        "name": "Lilo",
+        "species": "dog",
+        "carry": "p",
+        "weight": 5,
+        "date_of_birth": "1993-12-12 10:10"
+      }
+    ]
   }
-]
-}
-]];
+];
 
 router.get('/', (req, res) => {
   res.send('Acessando informações:\n' + JSON.stringify(users, null, 2));
@@ -29,7 +29,6 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   const userId = req.body.id;
 
-  
   const existingUser = users.find(user => user.id === userId);
 
   if (existingUser) {
@@ -46,7 +45,6 @@ router.post('/', (req, res) => {
     };
 
     users.push(user);
-
 
     res.redirect(`/tutor/${userId}`);
   }
@@ -68,8 +66,14 @@ router.patch('/:id', (req, res) => {
   const user = users.find(user => user.id === userId);
 
   if (user) {
-  
-    res.send('Atualizando usuário com o id: ' + userId);
+    user.name = req.body.name;
+    user.phone = req.body.phone;
+    user.email= req.body.email,
+    user.date_of_birth= req.body.date_of_birth,
+    user.zip_code= req.body.zip_code,
+    user.pets= req.body.pets || []
+
+    res.send('Usuário atualizado com sucesso');
   } else {
     res.status(404).send('Usuário não encontrado');
   }
@@ -103,9 +107,8 @@ router.delete('/:id', (req, res) => {
   const userIndex = users.findIndex(user => user.id === userId);
 
   if (userIndex !== -1) {
-    const deletedUser = users.splice(userIndex, 1);
-
-    res.send('Usuário excluído:\n' + JSON.stringify(deletedUser, null, 2));
+    users.splice(userIndex, 1);
+    res.send('Usuário removido com sucesso');
   } else {
     res.status(404).send('Usuário não encontrado');
   }
